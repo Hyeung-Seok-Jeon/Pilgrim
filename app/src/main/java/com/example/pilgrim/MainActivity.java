8ac3d9a948d92c0f154b7bc1fa1785ccd2c6145d
KingJade <shyboycym0711@gmail.com>
2019-12-02 22:39:52 +0900

    idchk update

--- app/src/main/java/com/example/pilgrim/MainActivity.java
+++ app/src/main/java/com/example/pilgrim/MainActivity.java
@@ -28,7 +28,7 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
-    //testTt
+    //testTtttttttttttttt
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
