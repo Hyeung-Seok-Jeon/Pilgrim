692cc1333e7abc8768eb8a7508ec5575d785cecf
HyeongSeok <wjswls279@naver.com>
2019-12-12 14:55:11 +0900

    update4

--- app/src/main/java/com/example/pilgrim/MainActivity.java
+++ app/src/main/java/com/example/pilgrim/MainActivity.java
@@ -1,17 +1,26 @@
 package com.example.pilgrim;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.Cursor;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.loader.content.CursorLoader;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -21,23 +30,68 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.target.SimpleTarget;
 import com.bumptech.glide.request.transition.Transition;
 import com.example.pilgrim.OpenApiParse.OpenApiParsingTask;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
-            private AppBarConfiguration mAppBarConfiguration;
-
-            @Override
+    private AppBarConfiguration mAppBarConfiguration;
+    int GALLERY_CODE=0;
+    private String imagePath;
+    private String filename;
+    private FirebaseStorage storage;
+    private DatabaseReference myRef;
+    private FirebaseDatabase database;
+    private SharedPreferences pref;
+    private String name;
+    private StorageReference storageRef,desertRef;
+    List<ImageDTO> personalimage=new ArrayList<>();
+    @Override
             protected void onCreate(Bundle savedInstanceState) {
                 super.onCreate(savedInstanceState);
                 setContentView(R.layout.activity_main);
                 //  setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);//정방향 세로로 완전히 고정,회전불가
                 Toolbar toolbar = findViewById(R.id.toolbar);
-
+                pref = getSharedPreferences("PrefName", Activity.MODE_PRIVATE);
+                storage=FirebaseStorage.getInstance();
+                database=FirebaseDatabase.getInstance();
+                storageRef=storage.getReference();
+                desertRef=storageRef.child(name+"file/");
+                myRef=database.getReference("file"+name);
                 setSupportActionBar(toolbar);
-
+                name=pref.getString("name","");
                 DrawerLayout drawer = findViewById(R.id.drawer_layout);
                 NavigationView navigationView = findViewById(R.id.nav_view);
+                database.getReference("file"+name).addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        personalimage.clear();
+                        for(DataSnapshot snapshot : dataSnapshot.getChildren()){
+                            ImageDTO imageDTO=snapshot.getValue(ImageDTO.class);
+                            personalimage.add(imageDTO);
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
                 // Passing each menu ID as a set of Ids because each
                 // menu should be considered as top level destinations.
                 mAppBarConfiguration = new AppBarConfiguration.Builder(
@@ -48,7 +102,22 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
         final View headerView=navigationView.getHeaderView(0);
-        Glide.with(this).load(R.drawable.prilgrimicon).into(new SimpleTarget<Drawable>() {
+        headerView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                desertRef.delete().addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+
+                    }
+                });
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+                startActivityForResult(intent,GALLERY_CODE);
+            }
+        });
+
+        Glide.with(this).load(personalimage.get(personalimage.size()-1).imageUrI).into(new SimpleTarget<Drawable>() {
             @Override
             public void onResourceReady(@NonNull Drawable resource, @Nullable Transition<? super Drawable> transition) {
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
@@ -59,7 +128,24 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 0 && resultCode == RESULT_OK){
+            imagePath=getPath(data.getData());
 
+
+        }
+    }
+    public String getPath(Uri uri){
+        String[] proj={MediaStore.Images.Media.DATA};
+        CursorLoader cursorLoader=new CursorLoader(this,uri,proj,null,null,null);
+        Cursor cursor=cursorLoader.loadInBackground();
+        assert cursor != null;
+        int index=cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        return cursor.getString(index);
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -72,6 +158,67 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+    private void uploadFile(String uri) {
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("업로드중...");
+        if (imagePath != null)
+            progressDialog.show();
+        else
+            Toast.makeText(this, "먼저 이미지를 선택해주세요", Toast.LENGTH_SHORT).show();
+
+
+        StorageReference storageRef = storage.getReferenceFromUrl("gs://pilgrim-16e7a.appspot.com/");
+        Uri file = Uri.fromFile(new File(uri));
+        filename = file.getLastPathSegment();
+        StorageReference riversRef = storageRef.child(name+"file/");
+        UploadTask uploadTask = riversRef.putFile(file);
+
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                progressDialog.dismiss();
+                Toast.makeText(getApplicationContext(), "업로드 실패!", Toast.LENGTH_SHORT).show();
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                progressDialog.dismiss(); //업로드 진행 Dialog 상자 닫기
+                Toast.makeText(getApplicationContext(), "데이터베이스 업로드 완료!", Toast.LENGTH_SHORT).show();
+                imgDeliver(filename);
+
+
+            }
+        }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                double progress = (100 * taskSnapshot.getBytesTransferred()) / taskSnapshot.getTotalByteCount();
+                progressDialog.setMessage("Uploaded " + ((int) progress) + "% ...");
+
+            }
+        });
+
+
+    }
+    private void imgDeliver(String name){
+        final FirebaseStorage storage = FirebaseStorage.getInstance();
+        final StorageReference storageRef = storage.getReferenceFromUrl("gs://pilgrim-16e7a.appspot.com/").child("images");
+        storageRef.child(name).getDownloadUrl().addOnCompleteListener(new OnCompleteListener<Uri>() {
+            @Override
+            public void onComplete(@NonNull Task<Uri> task) {
+                Uri downloadUri=task.getResult();
+                ImageDTO imageDTO=new ImageDTO();
+                assert downloadUri != null;
+                imageDTO.imageUrI=downloadUri.toString();
+                myRef.push().setValue(imageDTO);
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+            }
+        });
     }
 
 }
